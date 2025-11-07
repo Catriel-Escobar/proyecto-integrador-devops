4410932267a9a04c70f9a86dfb4fd94d54b8809d
Catriel <catriellescobar@gmail.com>
2025-11-07 00:03:48 -0300

    fix main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -2,10 +2,12 @@ terraform {
   required_providers {
     render = {
       source  = "render-oss/render"
-      version = "~> 0.4.0"
+      version = "~> 0.1.0"
     }
   }
+  required_version = ">= 1.3.0"
 }
+
 
 provider "render" {
   api_key = var.render_api_key
