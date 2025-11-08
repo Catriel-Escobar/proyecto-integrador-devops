db87f8b62331ffadae7f2c0c49a835878555fcfc
Catriel <catriellescobar@gmail.com>
2025-11-08 03:04:16 -0300

    Configuracion para grafana con prometheus (Cloud)

--- server.js
+++ server.js
@@ -1,17 +1,82 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-const cors = require('cors');
-const path = require('path');
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import cors from 'cors';
+import path from 'path';
+import clientProm from 'prom-client';
+import pkg, { pushMetrics } from 'prometheus-remote-write';
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+
+global.fetch = fetch;
+dotenv.config();
+
+const { RemoteWriteClient } = pkg;
+const collectDefaultMetrics = clientProm.collectDefaultMetrics;
+collectDefaultMetrics({ timeout: 5000 });
+const httpRequestDurationMicroseconds = new clientProm.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [50, 100, 200, 300, 500, 1000],
+});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 const DB_NAME = 'muro_comentarios';
 const COLLECTION_NAME = 'comentarios';
-
+console.log('MONGODB_URI:', MONGODB_URI);
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on('finish', () => {
+    end({
+      method: req.method,
+      route: req.route ? req.route.path : req.path,
+      code: res.statusCode,
+    });
+  });
+  next();
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', clientProm.register.contentType);
+  res.end(await clientProm.register.metrics());
+});
+
+const username = process.env.GRAFANA_USERNAME;
+const password = process.env.GRAFANA_API_KEY;
+console.log('username:', username);
+console.log('password:', password);
+const url =
+  'https://prometheus-prod-40-prod-sa-east-1.grafana.net/api/prom/push';
+
+async function enviarMetricas() {
+  try {
+    const metrics = await clientProm.register.getMetricsAsJSON();
+
+    const metricMap = {};
+    for (const m of metrics) {
+      if (m.type === 'gauge' || m.type === 'counter') {
+        metricMap[m.name] = m.values[0]?.value ?? 0;
+      }
+    }
+    await pushMetrics(metricMap, {
+      url,
+      auth: { username, password },
+      labels: { service: 'express-backend' },
+    });
+
+    console.log('Métricas enviadas correctamente a Grafana Cloud');
+  } catch (err) {
+    console.error(' Error enviando métricas:', err.message);
+  }
+}
+
+setInterval(enviarMetricas, 60000);
 
 let db;
 let client;
